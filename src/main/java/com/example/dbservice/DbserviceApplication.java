b88da937385a27282bbb0f35afe0d249a57d8b4e
unknown <Nikita_Karchahin@epam.com>
2020-09-03 13:36:21 +0300

    project is created

---
+++ src/main/java/com/example/dbservice/DbserviceApplication.java
@@ -0,0 +1,36 @@
+package com.example.dbservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+public class DbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbserviceApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			for (String arg : args) {
+				System.out.println(arg);
+			}
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+
+
+		};
+	}
+
+}
